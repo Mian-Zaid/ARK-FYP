cd4b9b5b93a53defb87775ca1f13934a2d317a5e
Mian Zaid <38668413+Mian-Zaid@users.noreply.github.com>
2020-12-30 00:33:10 +0500

    Update display3dModel.java

--- app/src/main/java/com/app/ark/fyp/display3dModel.java
+++ app/src/main/java/com/app/ark/fyp/display3dModel.java
@@ -2,6 +2,7 @@ package com.app.ark.fyp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -34,11 +35,15 @@ import java.util.function.Consumer;
 
 public class display3dModel extends AppCompatActivity {
     String modelName = null;
+    MediaPlayer player;
+    FirebaseStorage storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display3d_model);
+
+        //get model name from previous activity
 
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
@@ -47,16 +52,91 @@ public class display3dModel extends AppCompatActivity {
             modelName = null;
         }
 
+        //firebase storage
         FirebaseApp.initializeApp(this);
-        FirebaseStorage storage = FirebaseStorage.getInstance();
+        storage = FirebaseStorage.getInstance();
 
-        Button btn = findViewById(R.id.btn);
 
+        // get models from firebase Database
+        getModels();
+
+        // AR fragment to diaplay 3d model
         final ArFragment arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
 
-        btn.setOnClickListener(v -> {
-            try {
-                if (modelName != null) {
+        assert arFragment != null;
+
+        //tap on AR plane to render model on it
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+
+            //display models based on numbers
+
+            if (modelName.equals("3")) {
+                for (int i = 0; i < 3; ++i) {
+                    displayModel(hitResult, arFragment);
+                }
+                //background sound of animals
+                playSounds("tiger");
+
+            } else if (modelName.equals("5")) {
+                for (int i = 0; i < 5; ++i) {
+                    displayModel(hitResult, arFragment);
+                }
+                //background sound of animals
+                playSounds("elephent");
+
+            } else if (modelName.equals("8")) {
+
+                for (int i = 0; i < 8; ++i) {
+                    displayModel(hitResult, arFragment);
+                }
+
+            } else {
+                // single model display
+                displayModel(hitResult, arFragment);
+                //background sound of animals
+                playSounds(modelName.toLowerCase());
+            }
+
+
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopPlayer();
+    }
+
+    void getModels() {
+        try {
+            if (modelName != null) {
+
+                //model of Numbers
+
+                if (modelName.equals("3")) {
+                    Toast.makeText(this, "" + modelName.toLowerCase(), Toast.LENGTH_SHORT).show();
+                    File file = File.createTempFile("tiger", "glb");
+                    String modl = "tiger" + ".glb";
+                    StorageReference modelRef = storage.getReference().child(modl);
+                    modelRef.getFile(file).addOnSuccessListener(taskSnapshot -> buildModel(file));
+                } else if (modelName.equals("5")) {
+                    Toast.makeText(this, "" + modelName.toLowerCase(), Toast.LENGTH_SHORT).show();
+                    File file = File.createTempFile("elephent", "glb");
+                    String modl = "elephent" + ".glb";
+                    StorageReference modelRef = storage.getReference().child(modl);
+                    modelRef.getFile(file).addOnSuccessListener(taskSnapshot -> buildModel(file));
+                } else if (modelName.equals("8")) {
+                    Toast.makeText(this, "" + modelName.toLowerCase(), Toast.LENGTH_SHORT).show();
+                    File file = File.createTempFile("cap", "glb");
+                    String modl = "cap" + ".glb";
+                    StorageReference modelRef = storage.getReference().child(modl);
+                    modelRef.getFile(file).addOnSuccessListener(taskSnapshot -> buildModel(file));
+                }
+
+                //model other than numbers
+
+                else {
+
                     Toast.makeText(this, "" + modelName.toLowerCase(), Toast.LENGTH_SHORT).show();
 
                     File file = File.createTempFile(modelName.toLowerCase(), "glb");
@@ -66,40 +146,79 @@ public class display3dModel extends AppCompatActivity {
                     modelRef.getFile(file).addOnSuccessListener(taskSnapshot -> buildModel(file));
                 }
 
-
-            } catch (IOException e) {
-                e.printStackTrace();
             }
-        });
-
-        assert arFragment != null;
-        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
-            Log.d("andaragaya", "yes planne");
-
-            AnchorNode anchorNode = new AnchorNode(hitResult.createAnchor());
-//            anchorNode.setRenderable(renderable);
 
 
-            // Create the transformable andy and add it to the anchor.
-            TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
-// Maxscale must be greater than minscale
-            node.getScaleController().setMaxScale(0.2f);
-            node.getScaleController().setMinScale(0.1f);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-            node.setParent(anchorNode);
-            node.setRenderable(renderable);
-            node.select();
-            arFragment.getArSceneView().getScene().addChild(anchorNode);
+    }
 
-        });
+    private void playSounds(String name) {
+        if (name.equals("fox")) {
+            startPlayer(R.raw.fox);
+        } else if (name.equals("elephent")) {
+            startPlayer(R.raw.elephant);
+        } else if (name.equals("tiger")) {
+            startPlayer(R.raw.tiger);
+        }
+    }
+
+    private void displayModel(HitResult hitResult, ArFragment arFragment) {
+
+        //create anchor point where we tapped
+
+        AnchorNode anchorNode = new AnchorNode(hitResult.createAnchor());
+
+        // Create the transformable andy and add it to the anchor.
+        TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+
+        //sacle size of 3d Model on screen
+
+        node.getScaleController().setMaxScale(0.2f);
+        node.getScaleController().setMinScale(0.1f);
+
+        //finally set node with 3d model on AR fragment
+        node.setParent(anchorNode);
+        node.setRenderable(renderable);
+        node.select();
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+
+    }
+
+
+    private void stopPlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+
+        }
+    }
+
+
+    private void pausePlayer() {
+
+        if (player != null) {
+            player.pause();
+        }
+    }
+
+    //start playing sound
+    private void startPlayer(int sound) {
+
+        if (player == null) {
+            player = MediaPlayer.create(this, sound);
+            player.setOnCompletionListener(mp -> stopPlayer());
+        }
+        player.start();
     }
 
     private ModelRenderable renderable;
 
     private void buildModel(File file) {
 
-        Log.d("andaragaya", "yes");
-        Log.d("andaragaya", String.valueOf(Uri.parse(file.getPath())));
+        //create renderable source
 
         RenderableSource renderableSource = RenderableSource
                 .builder()
@@ -107,16 +226,16 @@ public class display3dModel extends AppCompatActivity {
                 .setRecenterMode(RenderableSource.RecenterMode.ROOT)
                 .build();
 
+        //intialize renderable 3d model
         ModelRenderable
                 .builder()
                 .setSource(this, renderableSource)
                 .setRegistryId(Uri.parse(file.getPath()))
                 .build()
                 .thenAccept(modelRenderable -> {
-                    Log.d("andaragaya", "yes again");
-
                     Toast.makeText(display3dModel.this, "Model built", Toast.LENGTH_SHORT).show();
                     renderable = modelRenderable;
+
                 });
     }
 
